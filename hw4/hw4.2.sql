0ace9ae3d1281dcb35b868329dca6e7463715513
KabStas <71084585+KabStas@users.noreply.github.com>
2021-11-09 20:38:23 +0700

    changes after hw4 checking comments

--- hw4/hw4.2.sql
+++ hw4/hw4.2.sql
@@ -1,224 +1,195 @@
 create or replace function kabenyk_st.is_patient_already_recorded_as_func (
-    p_id_patient in number, p_id_ticket in number
+    p_id_patient in number,
+    p_id_ticket in number
 )
-return number
+return boolean
 as
     v_result number;
-    v_cursor sys_refcursor;
-
-    type record_1 is record (
-        id_patient number,
-        id_ticket number
-    );
-    v_record record_1;
 begin
-    dbms_output.put_line ('1. Checking patient journal...');
-    open v_cursor for
-        select pj.id_patient, pj.id_ticket
-        from kabenyk_st.patients_journals pj
-        where pj.id_patient = p_id_patient and
-              pj.id_ticket = p_id_ticket and
-              pj.id_journal_record_status != 3;
+    select count(*)
+    into v_result
+    from kabenyk_st.patients_journals pj
+    where pj.id_patient = p_id_patient
+        and pj.id_ticket = p_id_ticket
+        and pj.id_journal_record_status in (1,2);
 
-        fetch v_cursor into v_record;
-
-        if v_cursor%notfound then
-            v_result := 1;
-            dbms_output.put_line ('OK');
-        else
-            v_result := 0;
-            dbms_output.put_line ('Error. Patient is already recorded');
-        end if;
-    close v_cursor;
-
-    return v_result;
+    if (v_result != 0) then
+        dbms_output.put_line ('Error. Patient is already recorded');
+    end if;
+    return v_result > 0;
 end;
 
 create or replace function kabenyk_st.is_gender_matched_as_func (
-    p_id_patient in number, p_id_specialization in number
+    p_id_patient in number,
+    p_id_specialization in number
 )
-return number
+return boolean
 as
     v_result number;
-    v_id_patient_gender number;
-    v_id_specialization_gender number;
 begin
-    dbms_output.put_line ('2. Checking gender...');
+    select count(*)
+    into v_result
+    from kabenyk_st.patients p
+    join kabenyk_st.gender g
+        on p.id_gender = g.id_gender
+    join kabenyk_st.specializations s
+        on s.id_gender = g.id_gender
+    where p.id_patient = p_id_patient
+        and s.id_specialization = p_id_specialization
+        and (
+            p.id_gender = s.id_gender
+            or s.id_gender is null
+        );
 
-    select pt.id_gender into v_id_patient_gender
-    from kabenyk_st.patients pt
-    where pt.id_patient = p_id_patient;
-
-    select s.id_gender into v_id_specialization_gender
-    from kabenyk_st.specializations s
-    where s.id_specialization = p_id_specialization;
-
-    if v_id_patient_gender = v_id_specialization_gender or
-       v_id_specialization_gender is null then -- специализация для обоих полов имеет пустое значение в колонке "пол"
-        v_result := 1;
-        dbms_output.put_line ('OK');
-    else
-        v_result := 0;
-        dbms_output.put_line ('Error. Gender is not correct');
+    if v_result = 0 then
+        dbms_output.put_line ('Error. Gender not matched');
     end if;
 
-    return v_result;
+    return v_result > 0;
 end;
 
-create or replace function kabenyk_st.is_age_correct_as_func (
-    p_id_patient in number, p_id_specialization in number
+create or replace function kabenyk_st.calc_age_from_date_of_birth (
+    p_date in date
 )
 return number
 as
-    v_result number;
-    v_id_patient_age number;
-    v_min_and_max_age_of_specialization_cursor sys_refcursor;
-
-    type record_1 is record (
-        min_age number,
-        max_age number
-    );
-    v_record_1 record_1;
-
+    v_age number;
 begin
-    dbms_output.put_line ('3. Checking age...');
+    select months_between(sysdate, p_date)/12
+    into v_age
+    from dual;
 
-    select trunc(months_between(sysdate, pt.date_of_birth)/12) into v_id_patient_age
-    from kabenyk_st.patients pt
-    where pt.id_patient = p_id_patient;
+    return v_age;
+end;
 
-    open v_min_and_max_age_of_specialization_cursor for
-        select s.min_age, s.max_age
-        from kabenyk_st.specializations s
-        where s.id_specialization = p_id_specialization;
+create or replace function kabenyk_st.get_patient_by_id (
+    p_id_patient number
+)
+return kabenyk_st.patients%rowtype
+as
+    v_patient kabenyk_st.patients%rowtype;
+begin
+    select *
+    into v_patient
+    from kabenyk_st.patients p
+    where p.id_patient = p_id_patient;
 
-        fetch v_min_and_max_age_of_specialization_cursor into v_record_1;
+    return v_patient;
+end;
 
-        if v_id_patient_age between v_record_1.min_age and v_record_1.max_age then
-            v_result := 1;
-            dbms_output.put_line ('OK');
-        else
-            v_result := 0;
-            dbms_output.put_line ('Error. Age is not correct');
-        end if;
+create or replace function kabenyk_st.is_age_matched_as_func (
+    p_id_patient in number,
+    p_id_specialization in number
+)
+return boolean
+as
+    v_patient kabenyk_st.patients%rowtype;
+    v_age number;
+    v_result number;
+begin
+    v_patient := kabenyk_st.get_patient_by_id (
+        p_id_patient => p_id_patient
+    );
 
-    close v_min_and_max_age_of_specialization_cursor;
+    v_age := kabenyk_st.calc_age_from_date_of_birth (
+        p_date => v_patient.date_of_birth
+    );
 
-    return v_result;
+    select count(*)
+    into v_result
+    from kabenyk_st.specializations s
+    where s.id_specialization = p_id_specialization
+        and (s.min_age <= v_age or s.min_age is null)
+        and (s.max_age >= v_age or s.max_age is null);
+
+    if v_result = 0 then
+        dbms_output.put_line ('Error. Age not matched');
+    end if;
+
+    return v_result > 0;
 end;
 
 create or replace function kabenyk_st.is_ticket_open_as_func (
     p_id_ticket in number
 )
-return number
+return boolean
 as
-    v_result number;
     v_ticket_flag number;
 begin
-    dbms_output.put_line ('4. Checking ticket...');
-
     select t.id_ticket_flag into v_ticket_flag
     from kabenyk_st.tickets t
     where t.id_ticket = p_id_ticket;
 
-    if v_ticket_flag = 1 then
-        v_result := 1;
-        dbms_output.put_line ('OK');
-    else
-        v_result := 0;
+    if v_ticket_flag != 1 then
         dbms_output.put_line ('Error. Ticket is not open');
     end if;
 
-    return v_result;
+    return v_ticket_flag = 1;
 end;
 
 create or replace function kabenyk_st.is_time_correct_as_func (
     p_id_ticket in number
 )
-return number
+return boolean
 as
-    v_result number;
     v_begin_time date;
 begin
-    dbms_output.put_line ('5. Checking time...');
-
     select t.begin_time into v_begin_time
     from kabenyk_st.tickets t
     where t.id_ticket = p_id_ticket;
 
-    if v_begin_time > sysdate then
-        v_result := 1;
-        dbms_output.put_line ('OK');
-    else
-        v_result := 0;
+    if v_begin_time < sysdate then
         dbms_output.put_line ('Error. Time is not correct');
     end if;
 
-    return v_result;
+    return v_begin_time > sysdate;
 end;
 
 create or replace function kabenyk_st.is_doctor_marked_as_deleted_as_func (
     p_id_ticket in number
 )
-return number
+return boolean
 as
-    v_result number;
     v_is_doctor_deleted date;
 begin
-    dbms_output.put_line ('6. Checking doctor...');
-
     select d.data_of_record_deletion into v_is_doctor_deleted
     from kabenyk_st.tickets t
         join kabenyk_st.doctors d
             on t.id_doctor = d.id_doctor
     where t.id_ticket = p_id_ticket;
 
-    if v_is_doctor_deleted is null then
-        v_result := 1;
-        dbms_output.put_line ('OK');
-    else
-        v_result := 0;
+    if v_is_doctor_deleted is not null then
         dbms_output.put_line ('Error. Doctor marked as deleted');
     end if;
 
-    return v_result;
+    return v_is_doctor_deleted is not null;
 end;
 
 create or replace function kabenyk_st.is_specialization_marked_as_deleted_as_func (
     p_id_specialization in number
 )
-return number
+return boolean
 as
-    v_result number;
     v_is_specialization_deleted date;
 begin
-    dbms_output.put_line ('7. Checking specialization...');
-
     select s.data_of_record_deletion into v_is_specialization_deleted
     from kabenyk_st.specializations s
     where s.id_specialization = p_id_specialization;
 
-    if v_is_specialization_deleted is null then
-        v_result := 1;
-        dbms_output.put_line ('OK');
-    else
-        v_result := 0;
+    if v_is_specialization_deleted is not null then
         dbms_output.put_line ('Error. Specialization marked as deleted');
     end if;
 
-    return v_result;
+    return v_is_specialization_deleted is not null;
 end;
 
 create or replace function kabenyk_st.is_hospital_marked_as_deleted_as_func (
     p_id_ticket in number
 )
-return number
+return boolean
 as
-    v_result number;
     v_is_hospital_deleted date;
 begin
-    dbms_output.put_line ('8. Checking hospital...');
-
     select h.data_of_record_deletion into v_is_hospital_deleted
     from kabenyk_st.tickets t
         join kabenyk_st.doctors d
@@ -227,78 +198,134 @@ begin
             on d.id_hospital = h.id_hospital
     where t.id_ticket = p_id_ticket;
 
-    if v_is_hospital_deleted is null then
-        v_result := 1;
-        dbms_output.put_line ('OK');
-    else
-        v_result := 0;
+    if v_is_hospital_deleted is not null then
         dbms_output.put_line ('Error. Hospital marked as deleted');
     end if;
 
-    return v_result;
+    return v_is_hospital_deleted is not null;
 end;
 
 create or replace function kabenyk_st.is_patient_has_oms_as_func (
     p_id_patient in number
 )
-return number
+return boolean
 as
-    v_result number;
     v_presence_of_oms number;
 begin
-    dbms_output.put_line ('9. Checking OMS...');
-
     select dn.id_document into v_presence_of_oms
     from kabenyk_st.patients p
         join kabenyk_st.documents_numbers dn
             on p.id_patient = dn.id_patient
     where p.id_patient = p_id_patient;
 
-    if v_presence_of_oms = 4 then
-        v_result := 1;
-        dbms_output.put_line ('OK');
-    else
-        v_result := 0;
+    if v_presence_of_oms != 4 then
         dbms_output.put_line ('Error. No OMS');
     end if;
 
-    return v_result;
+    return v_presence_of_oms = 4;
 end;
 
-create or replace procedure kabenyk_st.outputting_result_as_procedure (
+create or replace function kabenyk_st.get_ticket_by_id(
+    p_id_ticket number
+)
+return kabenyk_st.tickets%rowtype
+as
+    v_ticket kabenyk_st.tickets%rowtype;
+begin
+    select *
+    into v_ticket
+    from kabenyk_st.tickets t
+    where t.id_ticket = p_id_ticket;
+
+    return v_ticket;
+end;
+/
+
+create or replace function kabenyk_st.get_doctor_by_id(
+    p_id_doctor number
+)
+return kabenyk_st.doctors%rowtype
+as
+    v_doctor kabenyk_st.doctors%rowtype;
+begin
+    select *
+    into v_doctor
+    from kabenyk_st.doctors d
+    where d.id_doctor = p_id_doctor
+        and d.data_of_record_deletion is null;
+
+    return v_doctor;
+end;
+
+create or replace function kabenyk_st.get_journal_by_id(
+    p_id_journal number
+)
+return kabenyk_st.patients_journals%rowtype
+as
+    v_journal kabenyk_st.patients_journals%rowtype;
+begin
+    select *
+    into v_journal
+    from kabenyk_st.patients_journals pj
+    where pj.id_journal = p_id_journal;
+
+    return v_journal;
+end;
+
+create or replace procedure kabenyk_st.outputting_result_as_proc (
     p_id_journal in number
 )
 as
-    v_patient_journal_cursor sys_refcursor;
-    type record_1 is record (
-        patient varchar2(100),
-        day_time date,
-        doctor varchar2(100)
-    );
-    v_record record_1;
-
+    v_journal kabenyk_st.patients_journals%rowtype;
+    v_patient kabenyk_st.patients%rowtype;
+    v_ticket kabenyk_st.tickets%rowtype;
+    v_doctor kabenyk_st.doctors%rowtype;
 begin
-    open v_patient_journal_cursor for
-        select
-            p.surname,
-            pj.day_time,
-            d.surname
-        from
-            kabenyk_st.patients_journals pj
-            join kabenyk_st.patients p
-                on pj.id_patient = p.id_patient
-            join kabenyk_st.tickets t
-                on pj.id_ticket = t.id_ticket
-            join kabenyk_st.doctors d
-                on t.id_doctor = d.id_doctor
-        where pj.id_journal = p_id_journal;
+    v_journal := kabenyk_st.get_journal_by_id (p_id_journal => p_id_journal);
+    v_patient := kabenyk_st.get_patient_by_id (p_id_patient => v_journal.id_patient);
+    v_ticket := kabenyk_st.get_ticket_by_id (p_id_ticket => v_journal.id_ticket);
+    v_doctor := kabenyk_st.get_doctor_by_id (p_id_doctor => v_ticket.id_doctor);
 
-        fetch v_patient_journal_cursor into v_record;
-        dbms_output.put_line ('Record successful');
-        dbms_output.put_line ('пациент '||v_record.patient|| ', записан на ' ||
-                                  to_char(v_record.day_time, 'dd.mm.yyyy hh24:mi') ||', доктор ' ||
-                                  v_record.doctor);
-    close v_patient_journal_cursor;
+    dbms_output.put_line ('Record successful');
+    dbms_output.put_line (
+        'пациент '||v_patient.surname|| ', записан на ' ||
+        to_char( v_journal.day_time, 'dd.mm.yyyy hh24:mi') ||', доктор ' ||
+        v_doctor.surname);
+end;
+
+create or replace procedure kabenyk_st.changing_ticket_flag_to_close_as_proc (
+    p_id_ticket in number
+)
+as
+begin
+    update kabenyk_st.tickets t
+    set t.id_ticket_flag = 2
+    where t.id_ticket = p_id_ticket;
+    commit;
+end;
+
+
+create or replace function kabenyk_st.inserting_in_journal_as_func (
+    p_id_ticket in number, p_id_patient in number
+)
+return number
+as
+    v_begin_time date;
+    v_id_journal number;
+begin
+    --забираем время из талона, чтобы вставить в insert
+    select t.begin_time into v_begin_time
+    from kabenyk_st.tickets t
+    where t.id_ticket = p_id_ticket;
+
+    insert into kabenyk_st.patients_journals (
+        id_journal_record_status, day_time, id_patient, id_ticket
+    )
+    values (1, v_begin_time, p_id_patient, p_id_ticket)
+    returning id_journal into v_id_journal;
+    commit;
+
+    return v_id_journal;
 end;
 
 declare
@@ -306,70 +333,50 @@ declare
     v_id_ticket number := 3;
     v_id_specialization number := 3;
     v_id_journal number;
-    v_begin_time date;
-    v_result_1 number;
-    v_result_2 number;
-    v_result_3 number;
-    v_result_4 number;
-    v_result_5 number;
-    v_result_6 number;
-    v_result_7 number;
-    v_result_8 number;
-    v_result_9 number;
-
 begin
-    v_result_1 := kabenyk_st.is_patient_already_recorded_as_func (
-        p_id_patient => v_id_patient, p_id_ticket => v_id_ticket
-    );
-    v_result_2 := kabenyk_st.is_gender_matched_as_func (
-        p_id_patient => v_id_patient, p_id_specialization => v_id_specialization
-    );
-    v_result_3 := kabenyk_st.is_age_correct_as_func (
-        p_id_patient => v_id_patient, p_id_specialization => v_id_specialization
-    );
-    v_result_4 := kabenyk_st.is_ticket_open_as_func (
-        p_id_ticket => v_id_ticket
-    );
-    v_result_5 := kabenyk_st.is_time_correct_as_func (
-        p_id_ticket => v_id_ticket
-    );
-    v_result_6 := kabenyk_st.is_doctor_marked_as_deleted_as_func (
-        p_id_ticket => v_id_ticket
-    );
-    v_result_7 := kabenyk_st.is_specialization_marked_as_deleted_as_func (
-        p_id_specialization => v_id_specialization
-    );
-    v_result_8 := kabenyk_st.is_hospital_marked_as_deleted_as_func
-        (p_id_ticket => v_id_ticket
-    );
-    v_result_9 := kabenyk_st.is_patient_has_oms_as_func (
-        p_id_patient => v_id_patient
-    );
-
-    if v_result_1 = 1 and v_result_2 = 1 and v_result_3 = 1 and
-       v_result_4 = 1 and v_result_5 = 1 and v_result_6 = 1 and
-       v_result_7 = 1 and v_result_8 = 1 and v_result_9 = 1 then
-
-       update kabenyk_st.tickets t
-       set t.id_ticket_flag = 2
-       where t.id_ticket = v_id_ticket;
-
-       --забираем время из талона, чтобы вставить в insert
-       select t.begin_time into v_begin_time
-       from kabenyk_st.tickets t
-       where t.id_ticket = v_id_ticket;
-
-       insert into kabenyk_st.patients_journals (id_journal_record_status,
-                                                day_time, id_patient, id_ticket)
-       values (1, v_begin_time, v_id_patient, v_id_ticket)
-       returning id_journal into v_id_journal;
-       commit;
-
-       kabenyk_st.outputting_result_as_procedure (
-           p_id_journal => v_id_journal
+    if (
+        not kabenyk_st.is_patient_already_recorded_as_func(
+            p_id_patient => v_id_patient,
+            p_id_ticket => v_id_ticket
+        )
+        and kabenyk_st.is_gender_matched_as_func(
+            p_id_patient => v_id_patient,
+            p_id_specialization => v_id_specialization
+        )
+        and kabenyk_st.is_age_matched_as_func(
+            p_id_patient => v_id_patient,
+            p_id_specialization => v_id_specialization
+        )
+        and kabenyk_st.is_ticket_open_as_func(
+            p_id_ticket => v_id_ticket
+        )
+        and kabenyk_st.is_time_correct_as_func(
+            p_id_ticket => v_id_ticket
+        )
+        and not kabenyk_st.is_doctor_marked_as_deleted_as_func(
+            p_id_ticket => v_id_ticket
+        )
+        and not kabenyk_st.is_specialization_marked_as_deleted_as_func(
+            p_id_specialization => v_id_specialization
+        )
+        and not kabenyk_st.is_hospital_marked_as_deleted_as_func(
+            p_id_ticket => v_id_ticket
+        )
+        and kabenyk_st.is_patient_has_oms_as_func(
+            p_id_patient => v_id_patient
+        )
+    ) then
+        kabenyk_st.changing_ticket_flag_to_close_as_proc (
+            p_id_ticket => v_id_ticket
         );
-
+        v_id_journal := kabenyk_st.inserting_in_journal_as_func (
+            p_id_ticket => v_id_ticket,
+            p_id_patient => v_id_patient
+        );
+        kabenyk_st.outputting_result_as_procedure (
+            p_id_journal => v_id_journal
+        );
     else
-        dbms_output.put_line ('Error. Record unsuccessful');
+        dbms_output.put_line ('Record unsuccessful');
     end if;
 end;
--- hw4/hw4.3.sql
+++ hw4/hw4.3.sql
@@ -1,45 +1,34 @@
 create or replace function kabenyk_st.is_time_for_cancelling_correct_as_func (
     p_id_ticket in number
 )
-return number
+return boolean
 as
-    v_result number;
     v_begin_time date;
 begin
-    dbms_output.put_line ('1. Checking time...');
-
     select t.begin_time into v_begin_time
     from kabenyk_st.tickets t
     where t.id_ticket = p_id_ticket;
 
-    if v_begin_time > sysdate then
-        v_result := 1;
-        dbms_output.put_line ('OK');
-    else
-        v_result := 0;
+    if v_begin_time < sysdate then
         dbms_output.put_line ('Error. Time is not correct');
     end if;
 
-    return v_result;
+    return v_begin_time > sysdate;
 end;
 
 create or replace function kabenyk_st.is_hospital_still_working_as_func (
     p_id_ticket in number
 )
-return number
+return boolean
 as
-    v_result number := 1;
     v_current_day varchar2(100);
     v_current_time varchar2(5);
     v_hospital_end_time varchar2(20);
     v_number_of_day number;
 begin
-    dbms_output.put_line ('2. Checking hospital working time...');
-
     v_current_day := to_char(sysdate,'day');
     v_number_of_day := to_char(sysdate,'d');
     v_current_time := to_number(to_char(sysdate,'hh24.mi'), '99.99');
-    dbms_output.put_line (v_current_time);
 
     select wt.end_time into v_hospital_end_time
     from kabenyk_st.tickets t
@@ -47,7 +36,6 @@ begin
             on t.id_doctor = d.id_doctor
         join kabenyk_st.working_time wt
             on d.id_hospital = wt.id_hospital
-    --where t.id_ticket = p_id_ticket and wt.day = v_current_day;  --не работает!!!
     where t.id_ticket = p_id_ticket and
           decode (wt.day, 'Понедельник', 1,
                           'Вторник',2,
@@ -59,85 +47,79 @@ begin
 
     v_hospital_end_time := to_number(v_hospital_end_time, '99.99');
 
-    if (v_hospital_end_time - v_current_time) > 2 then
-        v_result := 1;
-        dbms_output.put_line ('OK');
-    else
-        v_result := 0;
+    if (v_hospital_end_time - v_current_time) < 2 then
         dbms_output.put_line ('Error. Less 2 hours of hospital work');
     end if;
 
-    return v_result;
+    return (v_hospital_end_time - v_current_time) >= 2;
 end;
 
-create or replace procedure kabenyk_st.outputting_result_as_proc (
+create or replace procedure kabenyk_st.outputting_result_of_deletion_as_proc (
     p_id_journal in number
 )
 as
-    v_patient_journal_cursor sys_refcursor;
-    type record_1 is record (
-        patient varchar2(100),
-        day_time date,
-        doctor varchar2(100)
-    );
-    v_record record_1;
-
+    v_journal kabenyk_st.patients_journals%rowtype;
+    v_patient kabenyk_st.patients%rowtype;
+    v_ticket kabenyk_st.tickets%rowtype;
+    v_doctor kabenyk_st.doctors%rowtype;
 begin
+    v_journal := kabenyk_st.get_journal_by_id (p_id_journal => p_id_journal);
+    v_patient := kabenyk_st.get_patient_by_id (p_id_patient => v_journal.id_patient);
+    v_ticket := kabenyk_st.get_ticket_by_id (p_id_ticket => v_journal.id_ticket);
+    v_doctor := kabenyk_st.get_doctor_by_id (p_id_doctor => v_ticket.id_doctor);
 
-    open v_patient_journal_cursor for
-        select
-            p.surname,
-            pj.day_time,
-            d.surname
-        from
-            kabenyk_st.patients_journals pj
-            join kabenyk_st.patients p
-                on pj.id_patient = p.id_patient
-            join kabenyk_st.tickets t
-                on pj.id_ticket = t.id_ticket
-            join kabenyk_st.doctors d
-                on t.id_doctor = d.id_doctor
-        where pj.id_journal = p_id_journal;
-
-        fetch v_patient_journal_cursor into v_record;
-        dbms_output.put_line ('Deletion successful');
-        dbms_output.put_line ('Запись пациента '||v_record.patient|| ' на ' ||
-                                  to_char(v_record.day_time, 'dd.mm.yyyy hh24:mi') ||' к доктору ' ||
-                                  v_record.doctor || ' отменена');
-    close v_patient_journal_cursor;
+    dbms_output.put_line ('Deletion successful');
+    dbms_output.put_line (
+        'Запись пациента '||v_patient.surname|| ' на ' ||
+        to_char( v_journal.day_time, 'dd.mm.yyyy hh24:mi') ||' к доктору ' ||
+        v_doctor.surname || ' отменена');
 end;
 
+create or replace procedure kabenyk_st.deletion_from_journal_as_proc (
+    p_id_journal in number
+)
+as
+begin
+    update kabenyk_st.patients_journals pj
+    set pj.id_journal_record_status = 3,
+        pj.data_of_record_deletion = sysdate
+    where pj.id_journal = p_id_journal;
+end;
+
+create or replace procedure kabenyk_st.changing_ticket_flag_to_open_as_proc (
+    p_id_ticket in number
+)
+as
+begin
+    update kabenyk_st.tickets t
+    set t.id_ticket_flag = 1
+    where t.id_ticket = p_id_ticket;
+    commit;
+end;
 
 declare
-    v_id_journal number := 9;
+    v_id_journal number := 13;
     v_id_ticket number := 3;
-    v_result_1 number;
-    v_result_2 number;
 begin
-    v_result_1 := kabenyk_st.is_time_for_cancelling_correct_as_func (
-        p_id_ticket => v_id_ticket
-    );
-    v_result_2 := kabenyk_st.is_hospital_still_working_as_func (
-        p_id_ticket => v_id_ticket
-    );
-
-    if v_result_1 = 1 and v_result_2 = 1 then
-
-        update kabenyk_st.patients_journals pj
-        set pj.id_journal_record_status = 3,
-            pj.data_of_record_deletion = sysdate
-        where pj.id_journal = v_id_journal;
-
-        update kabenyk_st.tickets t
-        set t.id_ticket_flag = 1
-        where t.id_ticket = v_id_ticket;
-        commit;
-
-        kabenyk_st.outputting_result_as_proc (
+    if (
+        kabenyk_st.is_time_for_cancelling_correct_as_func(
+            p_id_ticket => v_id_ticket
+        )
+        and kabenyk_st.is_hospital_still_working_as_func(
+            p_id_ticket => v_id_ticket
+        )
+    ) then
+        kabenyk_st.deletion_from_journal_as_proc (
+            p_id_journal => v_id_journal
+        );
+        kabenyk_st.changing_ticket_flag_to_open_as_proc (
+            p_id_ticket => v_id_ticket
+        );
+        kabenyk_st.outputting_result_of_deletion_as_proc (
             p_id_journal => v_id_journal
         );
     else
-        dbms_output.put_line ('Error. Deletion unsuccessful');
+        dbms_output.put_line ('Deletion unsuccessful');
     end if;
 
 end;
